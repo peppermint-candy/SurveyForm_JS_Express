75673f17e7abfa86ce073a968001af6f04f63647
Mint <273greenst@gmail.com>
2016-07-08 10:14:04 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+// require express
+var express = require("express");
+// path module -- try to figure out where and why we use this
+var path = require("path");
+var session = require('express-session')
+// create the express app
+var app = express();
+
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+app.use(session({secret: 'helloworld'}));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// root route to render the index.ejs view
+
+app.get('/', function(req, res) {
+ res.render("index");
+})
+// post route for adding a user
+app.post('/users', function(req, res) {
+ console.log("POST DATA", req.body);
+
+ req.session.name = req.body.name;
+ req.session.location = req.body.location
+ req.session.language = req.body.language
+ req.session.comment = req.body.comment
+
+ // This is where we would add the user to the database
+ // Then redirect to the root route
+ res.redirect('/results');
+})
+
+app.get('/results', function(req,res) {
+	console.log(req.session.name);
+	res.render("result", { session : req.session});
+})
+
+// tell the express app to listen on port 8000
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+});
